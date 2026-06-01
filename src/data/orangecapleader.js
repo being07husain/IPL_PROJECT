97e06a41a893f0eafdf9aeac17ec930cad5d38eb
Shubbu Husain <shubbuhusain2007@gmail.com>
2026-06-01 18:22:25 +0530

    UPLOAD JS FILE

---
+++ src/data/orangecapleader.js
@@ -0,0 +1,266 @@
+import OrangeCap2008 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2008.jpeg";
+import OrangeCap2009 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2009.jpeg";
+import OrangeCap2010 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2010.jpg";
+import OrangeCap2011 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2011.jpg";
+import OrangeCap2012 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2012.jpg";
+import OrangeCap2013 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2013.jpeg";
+import OrangeCap2014 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2014.jpeg";
+import OrangeCap2015 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2015.jpeg";
+import OrangeCap2016 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2016.jpeg";
+import OrangeCap2017 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2017.jpeg";
+import OrangeCap2018 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2018.jpg";
+import OrangeCap2019 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2019.jpeg";
+import OrangeCap2020 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2020.jpeg";
+import OrangeCap2021 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2021.jpg";
+import OrangeCap2022 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2022.jpeg";
+import OrangeCap2023 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2023.jpeg";
+import OrangeCap2024 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2024.jpg";
+import OrangeCap2025 from "../assets/SEASONS/ORANGECAPHOLDERS/OrangeCap2025.jpeg";
+import OrangeCap2026 from "https://ipl-stats-sports-mechanic.s3.ap-south-1.amazonaws.com/ipl/playerimages/Vaibhav%20Sooryavanshi.png"
+const iplTop10Batsmen = {
+  2008: [
+    { rank: 1, player: "Shaun Marsh", team: "Kings XI Punjab", runs: 616, img: OrangeCap2008 },
+    { rank: 2, player: "Gautam Gambhir", team: "Delhi Daredevils", runs: 534 },
+    { rank: 3, player: "Sanath Jayasuriya", team: "Mumbai Indians", runs: 514 },
+    { rank: 4, player: "Shane Watson", team: "Rajasthan Royals", runs: 472 },
+    { rank: 5, player: "Graeme Smith", team: "Rajasthan Royals", runs: 441 },
+    { rank: 6, player: "Adam Gilchrist", team: "Deccan Chargers", runs: 436 },
+    { rank: 7, player: "Yusuf Pathan", team: "Rajasthan Royals", runs: 435 },
+    { rank: 8, player: "Sachin Tendulkar", team: "Mumbai Indians", runs: 428 },
+    { rank: 9, player: "Suresh Raina", team: "Chennai Super Kings", runs: 421 },
+    { rank: 10, player: "MS Dhoni", team: "Chennai Super Kings", runs: 414 }
+  ],
+
+  2009: [
+    { rank: 1, player: "Matthew Hayden", team: "Chennai Super Kings", runs: 572, img: OrangeCap2009 },
+    { rank: 2, player: "Adam Gilchrist", team: "Deccan Chargers", runs: 495 },
+    { rank: 3, player: "Suresh Raina", team: "Chennai Super Kings", runs: 434 },
+    { rank: 4, player: "Tillakaratne Dilshan", team: "Delhi Daredevils", runs: 418 },
+    { rank: 5, player: "AB de Villiers", team: "Delhi Daredevils", runs: 465 },
+    { rank: 6, player: "Rohit Sharma", team: "Deccan Chargers", runs: 362 },
+    { rank: 7, player: "Sachin Tendulkar", team: "Mumbai Indians", runs: 364 },
+    { rank: 8, player: "Jacques Kallis", team: "Royal Challengers Bangalore", runs: 361 },
+    { rank: 9, player: "Herschelle Gibbs", team: "Deccan Chargers", runs: 371 },
+    { rank: 10, player: "Yuvraj Singh", team: "Kings XI Punjab", runs: 340 }
+  ],
+
+  2010: [
+    { rank: 1, player: "Sachin Tendulkar", team: "Mumbai Indians", runs: 618, img: OrangeCap2010 },
+    { rank: 2, player: "Jacques Kallis", team: "Royal Challengers Bangalore", runs: 572 },
+    { rank: 3, player: "Suresh Raina", team: "Chennai Super Kings", runs: 520 },
+    { rank: 4, player: "Murali Vijay", team: "Chennai Super Kings", runs: 458 },
+    { rank: 5, player: "Michael Hussey", team: "Chennai Super Kings", runs: 417 },
+    { rank: 6, player: "Rohit Sharma", team: "Deccan Chargers", runs: 404 },
+    { rank: 7, player: "Robin Uthappa", team: "Royal Challengers Bangalore", runs: 374 },
+    { rank: 8, player: "Virender Sehwag", team: "Delhi Daredevils", runs: 356 },
+    { rank: 9, player: "MS Dhoni", team: "Chennai Super Kings", runs: 287 },
+    { rank: 10, player: "David Warner", team: "Delhi Daredevils", runs: 282 }
+  ],
+
+  2011: [
+    { rank: 1, player: "Chris Gayle", team: "Royal Challengers Bangalore", runs: 608, img: OrangeCap2011 },
+    { rank: 2, player: "Sachin Tendulkar", team: "Mumbai Indians", runs: 553 },
+    { rank: 3, player: "Shaun Marsh", team: "Kings XI Punjab", runs: 504 },
+    { rank: 4, player: "Virender Sehwag", team: "Delhi Daredevils", runs: 424 },
+    { rank: 5, player: "Gautam Gambhir", team: "Kolkata Knight Riders", runs: 378 },
+    { rank: 6, player: "AB de Villiers", team: "Royal Challengers Bangalore", runs: 370 },
+    { rank: 7, player: "Mahela Jayawardene", team: "Mumbai Indians", runs: 352 },
+    { rank: 8, player: "Rohit Sharma", team: "Mumbai Indians", runs: 346 },
+    { rank: 9, player: "MS Dhoni", team: "Chennai Super Kings", runs: 337 },
+    { rank: 10, player: "Jacques Kallis", team: "Kolkata Knight Riders", runs: 328 }
+  ],
+
+  2012: [
+    { rank: 1, player: "Chris Gayle", team: "Royal Challengers Bangalore", runs: 733, img: OrangeCap2012 },
+    { rank: 2, player: "Gautam Gambhir", team: "Kolkata Knight Riders", runs: 590 },
+    { rank: 3, player: "Shikhar Dhawan", team: "Deccan Chargers", runs: 569 },
+    { rank: 4, player: "Ajinkya Rahane", team: "Rajasthan Royals", runs: 560 },
+    { rank: 5, player: "Virender Sehwag", team: "Delhi Daredevils", runs: 495 },
+    { rank: 6, player: "David Warner", team: "Delhi Daredevils", runs: 474 },
+    { rank: 7, player: "Robin Uthappa", team: "Kolkata Knight Riders", runs: 472 },
+    { rank: 8, player: "AB de Villiers", team: "Royal Challengers Bangalore", runs: 462 },
+    { rank: 9, player: "Rohit Sharma", team: "Mumbai Indians", runs: 453 },
+    { rank: 10, player: "Suresh Raina", team: "Chennai Super Kings", runs: 445 }
+  ],
+
+  2013: [
+    { rank: 1, player: "Michael Hussey", team: "Chennai Super Kings", runs: 733, img: OrangeCap2013 },
+    { rank: 2, player: "Chris Gayle", team: "Royal Challengers Bangalore", runs: 708 },
+    { rank: 3, player: "Rohit Sharma", team: "Mumbai Indians", runs: 538 },
+    { rank: 4, player: "Suresh Raina", team: "Chennai Super Kings", runs: 548 },
+    { rank: 5, player: "Robin Uthappa", team: "Pune Warriors India", runs: 434 },
+    { rank: 6, player: "David Warner", team: "Sunrisers Hyderabad", runs: 428 },
+    { rank: 7, player: "Virat Kohli", team: "Royal Challengers Bangalore", runs: 422 },
+    { rank: 8, player: "Shikhar Dhawan", team: "Sunrisers Hyderabad", runs: 415 },
+    { rank: 9, player: "AB de Villiers", team: "Royal Challengers Bangalore", runs: 405 },
+    { rank: 10, player: "Gautam Gambhir", team: "Kolkata Knight Riders", runs: 393 }
+  ],
+
+  2014: [
+    { rank: 1, player: "Robin Uthappa", team: "Kolkata Knight Riders", runs: 660, img: OrangeCap2014 },
+    { rank: 2, player: "Dwayne Smith", team: "Chennai Super Kings", runs: 566 },
+    { rank: 3, player: "Glenn Maxwell", team: "Kings XI Punjab", runs: 552 },
+    { rank: 4, player: "David Warner", team: "Sunrisers Hyderabad", runs: 528 },
+    { rank: 5, player: "Suresh Raina", team: "Chennai Super Kings", runs: 523 },
+    { rank: 6, player: "Virat Kohli", team: "Royal Challengers Bangalore", runs: 511 },
+    { rank: 7, player: "Rohit Sharma", team: "Mumbai Indians", runs: 496 },
+    { rank: 8, player: "Shikhar Dhawan", team: "Sunrisers Hyderabad", runs: 484 },
+    { rank: 9, player: "Raina Suresh", team: "Chennai Super Kings", runs: 478 },
+    { rank: 10, player: "Murali Vijay", team: "Chennai Super Kings", runs: 472 }
+  ],
+
+  2015: [
+    { rank: 1, player: "David Warner", team: "Sunrisers Hyderabad", runs: 562, img: OrangeCap2015 },
+    { rank: 2, player: "Lendl Simmons", team: "Mumbai Indians", runs: 540 },
+    { rank: 3, player: "Ajinkya Rahane", team: "Rajasthan Royals", runs: 540 },
+    { rank: 4, player: "AB de Villiers", team: "Royal Challengers Bangalore", runs: 513 },
+    { rank: 5, player: "Robin Uthappa", team: "Kolkata Knight Riders", runs: 364 },
+    { rank: 6, player: "Virat Kohli", team: "Royal Challengers Bangalore", runs: 357 },
+    { rank: 7, player: "Suresh Raina", team: "Chennai Super Kings", runs: 353 },
+    { rank: 8, player: "Shikhar Dhawan", team: "Sunrisers Hyderabad", runs: 352 },
+    { rank: 9, player: "Rohit Sharma", team: "Mumbai Indians", runs: 346 },
+    { rank: 10, player: "Steve Smith", team: "Rajasthan Royals", runs: 337 }
+  ],
+
+  2016: [
+    { rank: 1, player: "Virat Kohli", team: "Royal Challengers Bangalore", runs: 973, img: OrangeCap2016 },
+    { rank: 2, player: "David Warner", team: "Sunrisers Hyderabad", runs: 848 },
+    { rank: 3, player: "AB de Villiers", team: "Royal Challengers Bangalore", runs: 687 },
+    { rank: 4, player: "Shikhar Dhawan", team: "Sunrisers Hyderabad", runs: 501 },
+    { rank: 5, player: "Rohit Sharma", team: "Mumbai Indians", runs: 489 },
+    { rank: 6, player: "Steve Smith", team: "Rajasthan Royals", runs: 479 },
+    { rank: 7, player: "Ajinkya Rahane", team: "Rajasthan Royals", runs: 437 },
+    { rank: 8, player: "MS Dhoni", team: "Chennai Super Kings", runs: 429 },
+    { rank: 9, player: "Suresh Raina", team: "Gujarat Lions", runs: 422 },
+    { rank: 10, player: "Quinton de Kock", team: "Delhi Daredevils", runs: 418 }
+  ],
+
+  2017: [
+    { rank: 1, player: "David Warner", team: "Sunrisers Hyderabad", runs: 641, img: OrangeCap2017 },
+    { rank: 2, player: "Gautam Gambhir", team: "Kolkata Knight Riders", runs: 498 },
+    { rank: 3, player: "Shikhar Dhawan", team: "Sunrisers Hyderabad", runs: 479 },
+    { rank: 4, player: "Steve Smith", team: "Rising Pune Supergiant", runs: 472 },
+    { rank: 5, player: "Suresh Raina", team: "Gujarat Lions", runs: 442 },
+    { rank: 6, player: "Virat Kohli", team: "Royal Challengers Bangalore", runs: 440 },
+    { rank: 7, player: "Ambati Rayudu", team: "Mumbai Indians", runs: 408 },
+    { rank: 8, player: "AB de Villiers", team: "Royal Challengers Bangalore", runs: 404 },
+    { rank: 9, player: "Rohit Sharma", team: "Mumbai Indians", runs: 399 },
+    { rank: 10, player: "Ajinkya Rahane", team: "Rajasthan Royals", runs: 391 }
+  ],
+
+  2018: [
+    { rank: 1, player: "Kane Williamson", team: "Sunrisers Hyderabad", runs: 735, img: OrangeCap2018 },
+    { rank: 2, player: "Rishabh Pant", team: "Delhi Daredevils", runs: 684 },
+    { rank: 3, player: "KL Rahul", team: "Kings XI Punjab", runs: 659 },
+    { rank: 4, player: "Ambati Rayudu", team: "Chennai Super Kings", runs: 602 },
+    { rank: 5, player: "Shane Watson", team: "Chennai Super Kings", runs: 555 },
+    { rank: 6, player: "Rohit Sharma", team: "Mumbai Indians", runs: 538 },
+    { rank: 7, player: "David Warner", team: "Sunrisers Hyderabad", runs: 517 },
+    { rank: 8, player: "Shikhar Dhawan", team: "Delhi Daredevils", runs: 514 },
+    { rank: 9, player: "Virat Kohli", team: "Royal Challengers Bangalore", runs: 500 },
+    { rank: 10, player: "AB de Villiers", team: "Royal Challengers Bangalore", runs: 474 }
+  ],
+
+  2019: [
+    { rank: 1, player: "David Warner", team: "Sunrisers Hyderabad", runs: 692, img: OrangeCap2019 },
+    { rank: 2, player: "KL Rahul", team: "Kings XI Punjab", runs: 593 },
+    { rank: 3, player: "Quinton de Kock", team: "Mumbai Indians", runs: 529 },
+    { rank: 4, player: "Shikhar Dhawan", team: "Delhi Capitals", runs: 521 },
+    { rank: 5, player: "Andre Russell", team: "Kolkata Knight Riders", runs: 510 },
+    { rank: 6, player: "Rohit Sharma", team: "Mumbai Indians", runs: 480 },
+    { rank: 7, player: "Rishabh Pant", team: "Delhi Capitals", runs: 473 },
+    { rank: 8, player: "Steve Smith", team: "Rajasthan Royals", runs: 441 },
+    { rank: 9, player: "AB de Villiers", team: "Royal Challengers Bangalore", runs: 429 },
+    { rank: 10, player: "Suresh Raina", team: "Chennai Super Kings", runs: 419 }
+  ],
+
+  2020: [
+    { rank: 1, player: "KL Rahul", team: "Kings XI Punjab", runs: 670, img: OrangeCap2020 },
+    { rank: 2, player: "Shikhar Dhawan", team: "Delhi Capitals", runs: 618 },
+    { rank: 3, player: "David Warner", team: "Sunrisers Hyderabad", runs: 548 },
+    { rank: 4, player: "Ishan Kishan", team: "Mumbai Indians", runs: 516 },
+    { rank: 5, player: "Quinton de Kock", team: "Mumbai Indians", runs: 503 },
+    { rank: 6, player: "Faf du Plessis", team: "Chennai Super Kings", runs: 480 },
+    { rank: 7, player: "Suresh Raina", team: "Chennai Super Kings", runs: 472 },
+    { rank: 8, player: "Virat Kohli", team: "Royal Challengers Bangalore", runs: 460 },
+    { rank: 9, player: "Sanju Samson", team: "Rajasthan Royals", runs: 450 },
+    { rank: 10, player: "Rohit Sharma", team: "Mumbai Indians", runs: 441 }
+  ],
+
+  2021: [
+    { rank: 1, player: "Ruturaj Gaikwad", team: "Chennai Super Kings", runs: 635, img: OrangeCap2021 },
+    { rank: 2, player: "Faf du Plessis", team: "Chennai Super Kings", runs: 633 },
+    { rank: 3, player: "KL Rahul", team: "Punjab Kings", runs: 626 },
+    { rank: 4, player: "Shikhar Dhawan", team: "Delhi Capitals", runs: 587 },
+    { rank: 5, player: "Glenn Maxwell", team: "Royal Challengers Bangalore", runs: 513 },
+    { rank: 6, player: "Virat Kohli", team: "Royal Challengers Bangalore", runs: 405 },
+    { rank: 7, player: "David Warner", team: "Sunrisers Hyderabad", runs: 391 },
+    { rank: 8, player: "Ishan Kishan", team: "Mumbai Indians", runs: 379 },
+    { rank: 9, player: "Suresh Raina", team: "Chennai Super Kings", runs: 368 },
+    { rank: 10, player: "Rohit Sharma", team: "Mumbai Indians", runs: 349 }
+  ],
+
+  2022: [
+    { rank: 1, player: "Jos Buttler", team: "Rajasthan Royals", runs: 863, img: OrangeCap2022 },
+    { rank: 2, player: "KL Rahul", team: "Lucknow Super Giants", runs: 616 },
+    { rank: 3, player: "Quinton de Kock", team: "Lucknow Super Giants", runs: 508 },
+    { rank: 4, player: "Hardik Pandya", team: "Gujarat Titans", runs: 487 },
+    { rank: 5, player: "Shubman Gill", team: "Gujarat Titans", runs: 483 },
+    { rank: 6, player: "Virat Kohli", team: "Royal Challengers Bangalore", runs: 473 },
+    { rank: 7, player: "Faf du Plessis", team: "Royal Challengers Bangalore", runs: 461 },
+    { rank: 8, player: "Rohit Sharma", team: "Mumbai Indians", runs: 451 },
+    { rank: 9, player: "Sanju Samson", team: "Rajasthan Royals", runs: 445 },
+    { rank: 10, player: "Suresh Raina", team: "Chennai Super Kings", runs: 413 }
+  ],
+
+  2023: [
+    { rank: 1, player: "Shubman Gill", team: "Gujarat Titans", runs: 890, img: OrangeCap2023 },
+    { rank: 2, player: "Faf du Plessis", team: "Royal Challengers Bangalore", runs: 730 },
+    { rank: 3, player: "Devon Conway", team: "Chennai Super Kings", runs: 672 },
+    { rank: 4, player: "Virat Kohli", team: "Royal Challengers Bangalore", runs: 639 },
+    { rank: 5, player: "Yashasvi Jaiswal", team: "Rajasthan Royals", runs: 625 },
+    { rank: 6, player: "KL Rahul", team: "Lucknow Super Giants", runs: 597 },
+    { rank: 7, player: "Ishan Kishan", team: "Mumbai Indians", runs: 573 },
+    { rank: 8, player: "Hardik Pandya", team: "Gujarat Titans", runs: 557 },
+    { rank: 9, player: "David Warner", team: "Delhi Capitals", runs: 547 },
+    { rank: 10, player: "Ruturaj Gaikwad", team: "Chennai Super Kings", runs: 535 }
+  ],
+
+  2024: [
+    { rank: 1, player: "Virat Kohli", team: "Royal Challengers Bengaluru", runs: 741, img: OrangeCap2024 },
+    { rank: 2, player: "Ruturaj Gaikwad", team: "Chennai Super Kings", runs: 583 },
+    { rank: 3, player: "Riyan Parag", team: "Rajasthan Royals", runs: 573 },
+    { rank: 4, player: "Travis Head", team: "Sunrisers Hyderabad", runs: 567 },
+    { rank: 5, player: "Sanju Samson", team: "Rajasthan Royals", runs: 531 },
+    { rank: 6, player: "Shubman Gill", team: "Gujarat Titans", runs: 519 },
+    { rank: 7, player: "Rohit Sharma", team: "Mumbai Indians", runs: 507 },
+    { rank: 8, player: "Yashasvi Jaiswal", team: "Rajasthan Royals", runs: 497 },
+    { rank: 9, player: "Hardik Pandya", team: "Mumbai Indians", runs: 487 },
+    { rank: 10, player: "Faf du Plessis", team: "Royal Challengers Bengaluru", runs: 463 }
+  ],
+  2025: [
+    { rank: 1, name: 'Sai Sudharsan', team: "Gujarat Titans", runs: 759, img: OrangeCap2025 },
+    { rank: 2, name: 'Shubman Gill', team: "Gujarat Titans", runs: 753 },
+    { rank: 3, name: 'Virat Kohli', team: "Royal Challengers Bengaluru", runs: 745 },
+    { rank: 4, name: 'Rohit Sharma', team: "Mumbai Indians", runs: 728 },
+    { rank: 5, name: 'Jos Buttler', team: "Gujarat Titans", runs: 712 },
+    { rank: 6, name: 'KL Rahul', team: "Delhi Capitals", runs: 698 },
+    { rank: 7, name: 'Rishabh Pant', team: "Lucknow Super Giants", runs: 685 },
+    { rank: 8, name: 'Suryakumar Yadav', team: "Mumbai Indians", runs: 673 },
+    { rank: 9, name: 'Yashasvi Jaiswal', team: "Rajasthan Royals", runs: 662 },
+    { rank: 10, name: 'Heinrich Klaasen', team: "Sunrisers Hyderabad", runs: 651 }],
+  2026: [
+    { rank: 1, player: "Vaibhav Sooryavanshi", team: "Rajasthan Royals", runs: 776, img: OrangeCap2026 },
+    { rank: 2, player: "Shubman Gill", team: "Gujarat Titans", runs: 732 },
+    { rank: 3, player: "Sai Sudharsan", team: "Gujarat Titans", runs: 722 },
+    { rank: 4, player: "Virat Kohli", team: "Royal Challengers Bengaluru", runs: 675 },
+    { rank: 5, player: "Heinrich Klaasen", team: "Sunrisers Hyderabad", runs: 624 },
+    { rank: 6, player: "Ishan Kishan", team: "Sunrisers Hyderabad", runs: 602 },
+    { rank: 7, player: "KL Rahul", team: "Delhi Capitals", runs: 593 },
+    { rank: 8, player: "Abhishek Sharma", team: "Sunrisers Hyderabad", runs: 563 },
+    { rank: 9, player: "Mitchell Marsh", team: "Lucknow Super Giants", runs: 563 },
+    { rank: 10, player: "Jos Buttler", team: "Gujarat Titans", runs: 526 }
+  ]
+};
+
+export default iplTop10Batsmen;
